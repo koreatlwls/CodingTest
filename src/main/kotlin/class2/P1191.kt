9d8f84cf58a8337958f81ecfbb344d8dd0e6e4a8
koreatlwls <koreatlwls@gmail.com>
2022-12-19 21:48:21 +0900

    feat : 20221219

--- src/main/kotlin/class2/P1191.kt
+++ src/main/kotlin/class2/P1191.kt
@@ -1,2 +1,14 @@
 package class2
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+//fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+//
+//    val n = readLine().toInt()
+//
+//    val strings = Array(n) { readLine() }.distinct().sorted().sortedWith(compareBy { it.length })
+//    for (s in strings) {
+//        println(s)
+//    }
+//}
