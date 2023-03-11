86123f4326529568055e155cd3f5675dfcbd7887
koreatlwls <koreatlwls@gmail.com>
2023-03-11 15:05:27 +0900

    feat : 20230311 (프로그래머스 바탕화면 정리)

---
+++ src/main/kotlin/programmers/test01.kt
@@ -0,0 +1,28 @@
+package programmers
+
+import kotlin.math.*
+
+// 바탕화면 정리
+
+class Solution {
+    fun solution(wallpaper: Array<String>): IntArray {
+        var minRow = Int.MAX_VALUE
+        var minCol = Int.MAX_VALUE
+        var maxRow = Int.MIN_VALUE
+        var maxCol = Int.MIN_VALUE
+
+        val length = wallpaper[0].length
+        for (i in wallpaper.indices) {
+            for (j in 0 until length) {
+                if (wallpaper[i].get(j) == '#') {
+                    minRow = min(minRow, i)
+                    minCol = min(minCol, j)
+                    maxRow = max(maxRow, i)
+                    maxCol = max(maxCol, j)
+                }
+            }
+        }
+
+        return intArrayOf(minRow, minCol, maxRow + 1, maxCol + 1)
+    }
+}
