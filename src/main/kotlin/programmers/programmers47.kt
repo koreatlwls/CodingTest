1b5b69b444d3bf266e76781df9296af30ba4ca50
sijin.kim <sijin.kim@yanolja.com>
2024-03-21 22:15:25 +0900

    feat : n^2 배열 자르기

---
+++ src/main/kotlin/programmers/programmers47.kt
@@ -0,0 +1,20 @@
+package programmers
+
+class Solution47 {
+
+    fun solution(n: Int, left: Long, right: Long): IntArray {
+
+        var answer = IntArray((right - left).toInt() + 1)
+
+        var idx = left
+
+        for (i in answer.indices) {
+            val r = (idx / n.toLong()).toInt()
+            val c = (idx % n.toLong()).toInt()
+            answer[i] = r.coerceAtLeast(c) + 1
+            idx++
+        }
+
+        return answer
+    }
+}
