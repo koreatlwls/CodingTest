61bb53474e7a0d639936b9642dc2881321d1c6d8
koreatlwls <koreatlwls@gmail.com>
2024-10-01 23:05:39 +0900

    feat : problem3

---
+++ src/main/kotlin/problem3.kt
@@ -0,0 +1,12 @@
+import java.util.StringTokenizer
+
+private fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(' ').map { it.toInt() }
+    val sum = LongArray(n + 1)
+    StringTokenizer(readLine()).apply { repeat(n) { sum[it + 1] = sum[it] + nextToken().toLong() } }
+    var max = 0L
+    for (i in m..n) {
+        max = maxOf(max, sum[i] - sum[i - m])
+    }
+    println(max)
+}
